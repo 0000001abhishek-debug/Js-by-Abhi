ee8941f9327004ca255a1a5eca8b8d4e76918e65
Abhishek Kumar <yourgithubemail@gmail.com>
2026-02-16 14:50:29 +0530

    VARIABLE

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -27,3 +27,30 @@ let state;
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+//PRATICE 
+console.log(null + 1);
+console.log("5" + 3);
+console.log("5" - 3);
+console.log(true + false);
+
+console.log(typeof []);
+console.log(typeof null); // null bhi ha or object bhi ha js bug ha 
+console.log(typeof 123n);
+
+console.log(Boolean(0));
+console.log(Boolean("0"));
+console.log(Boolean([]));
+console.log(Boolean(undefined));
+
+
+function isEmpty(value) {
+  return value === null || value === undefined || value === "";
+}console.log(isEmpty(null));      // true
+console.log(isEmpty(undefined)); // true
+console.log(isEmpty(""));        // true
+console.log(isEmpty("hello"));   // false
+
+
+console.log(5 == "5");
+console.log(5 === "5");
